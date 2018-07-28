74a689a37f704f4682f64d72984aa5bdbebfd41e
Rikki Schulte <richard.schulte@ventera.hcqis.org>
2018-07-28 09:08:09 -0400

    reorganize

---
+++ src/map.js
@@ -0,0 +1,80 @@
+/* global mapboxgl */
+/* eslint-disable no-irregular-whitespace */
+/* eslint-disable no-undef */
+
+import popupComponent from './components/popup';
+
+import { MAP, LAYER_ID, ANIMATION_DURATION, ACCESS_TOKEN } from './config';
+
+mapboxgl.accessToken = ACCESS_TOKEN;
+
+const DEFAULT_MAP = {
+  container: 'map'
+};
+
+export function createCompass() {
+  const leftEl = document.querySelector('.mapboxgl-ctrl-bottom-left');
+  const compass = document.createElement('div');
+
+  compass.innerHTML = `<div class="mapboxgl-ctrl mapboxgl-ctrl-group">
+      <button class="mapboxgl-ctrl-icon mapboxgl-ctrl-compass" type="button" aria-label="Reset North"> 
+      <span class="mapboxgl-ctrl-compass-arrow" style="transform: rotate(0deg);"></span> 
+    </button> 
+  </div>`;
+
+  compass.onclick = () => {
+    map.flyTo({
+      center: DEFAULT_MAP.center
+    });
+  };
+  leftEl.appendChild(compass);
+}
+
+export default function getMap() {
+  const map = new mapboxgl.Map(
+    // Set defaults, allow the config to override
+    Object.assign(DEFAULT_MAP, MAP)
+  );
+
+  const popup = new mapboxgl.Popup();
+
+  // Create a mapboxgl.Popup from the default popup component.
+  const showPopup = feature => {
+    popup
+      .setLngLat(feature.geometry.coordinates)
+      .setHTML(popupComponent(feature))
+      .addTo(map);
+    popup.on('close', () => {
+      map.flyTo({
+        center: DEFAULT_MAP.center,
+        duration: ANIMATION_DURATION
+      });
+    });
+
+    return popup;
+  };
+
+  // HANDLE MAP EVENTS
+
+  map.on('click', LAYER_ID, e => {
+    const feature = e.features[0];
+    setTimeout(() => {
+      map.flyTo({
+        center: feature.geometry.coordinates,
+        duration: ANIMATION_DURATION
+      });
+      showPopup(feature);
+    }, 200);
+  });
+
+  //FULL SCREEN MODE
+
+  map.addControl(new mapboxgl.FullscreenControl());
+  // Add zoom conntrol
+  const nav = new mapboxgl.NavigationControl({ showCompass: false });
+  map.addControl(nav, 'top-left');
+  // disable scrollZoom
+  map.scrollZoom.disable();
+  createCompass();
+  return map;
+}
