c467e362cbaa68f3bb28182637e0ffebda0a3d49
Rikki Schulte <rikki@Rikkis-MacBook-Pro.local>
2018-08-01 16:59:28 -0400

    use padding

--- src/config/text.js
+++ src/config/text.js
@@ -1,6 +1,15 @@
-export default {
+const es = {
   LEGEND_TITLE: "Puestos Estaciónes y Tourismos",
   LEGEND_VIEW_ALL: "Ver Todo",
   POPUP_IMAGE_ALT_PREFIX: "Image del Estación",
   POPUP_MORE_INFO: "Más informacíon"
 };
+
+const en = {
+  LEGEND_TITLE: "English Legend Title",
+  LEGEND_VIEW_ALL: "View All",
+  POPUP_IMAGE_ALT_PREFIX: "Image of station",
+  POPUP_MORE_INFO: "More information"
+};
+
+export { es, es as default, en };
