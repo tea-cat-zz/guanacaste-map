08d70e2ac6743a6f5d998ab295e99d8a6419cc16
Rikki Schulte <rikki@Rikkis-MacBook-Pro.local>
2018-08-15 14:50:33 -0400

    revert popup navs

--- src/map.js
+++ src/map.js
@@ -1,4 +1,4 @@
-/* global mapboxgl */
+/* global mapboxgl, tcat */
 /* eslint-disable no-irregular-whitespace */
 /* eslint-disable no-undef */
 
@@ -14,13 +14,7 @@ import {
   getFilterLayerToggleHandler
 } from "./handlers";
 
-import {
-  MAP,
-  ANIMATION_DURATION,
-  ACCESS_TOKEN,
-  LAYERS,
-  LAYERS_ACTIVE
-} from "./config";
+import { MAP, ACCESS_TOKEN, LAYERS, LAYERS_ACTIVE } from "./config";
 
 mapboxgl.accessToken = ACCESS_TOKEN;
 
@@ -37,11 +31,11 @@ export const handleInitialLoad = map => {
   map.visibleLayers = getVisibleLayers(map.layerList, false);
   map.filteredLayers = getFilteredLayers(map.layerList);
   // HANDLE MAP LOAD
-  window.tcat.handleLayerToggle = getLayerToggleHandler(map);
-  window.tcat.handleFilterToggle = getFilterToggleHandler(map);
-  window.tcat.handleFilterLayerToggle = getFilterLayerToggleHandler(map);
-  window.tcat.handleShowAll = getShowAllHandler(map);
-  window.tcat.handleHideAll = getHideAllHandler(map);
+  tcat.handleLayerToggle = getLayerToggleHandler(map);
+  tcat.handleFilterToggle = getFilterToggleHandler(map);
+  tcat.handleFilterLayerToggle = getFilterLayerToggleHandler(map);
+  tcat.handleShowAll = getShowAllHandler(map);
+  tcat.handleHideAll = getHideAllHandler(map);
 };
 
 export const toggleLayer = (map, layerId, isVisible = true) => {
@@ -72,9 +66,7 @@ export default function getMap() {
     Object.assign(DEFAULT_MAP, MAP)
   );
 
-  const popup = new mapboxgl.Popup({
-    maxWidth: "240px"
-  });
+  const popup = new mapboxgl.Popup();
 
   // Create a mapboxgl.Popup from the default popup component.
   const showPopup = feature => {
@@ -84,7 +76,7 @@ export default function getMap() {
       .addTo(map);
     popup.on("close", () => {
       // map.flyTo({
-      //   center: DEFAULT_MAP.center,
+      //   center: MAP.center,
       //   duration: ANIMATION_DURATION
       // });
     });
@@ -99,13 +91,11 @@ export default function getMap() {
     .forEach(([layerId]) => {
       map.on("click", layerId, e => {
         const feature = e.features[0];
-        setTimeout(() => {
-          map.flyTo({
-            center: feature.geometry.coordinates,
-            duration: ANIMATION_DURATION
-          });
-          showPopup(feature);
-        }, 200);
+        // map.flyTo({
+        //   center: feature.geometry.coordinates,
+        //   duration: ANIMATION_DURATION
+        // });
+        showPopup(feature);
       });
     });
 
